f2c8f98308bea32ba193b2a86673410a14e2b50a
syazani <syazanihussin@gmail.com>
2018-02-19 23:20:18 +0800

    modified dice

--- application logics/functions/index.js
+++ application logics/functions/index.js
@@ -1,8 +1,27 @@
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+/********************************************************************************************
+ * Function 1: Take the text parameter passed to this HTTP endpoint and insert it into the  *
+ *             Realtime Database under the path /messages/:pushId/original                  *
+ ********************************************************************************************/
+exports.addParticipant = functions.https.onRequest((req, res) => {
+  const email = req.query.email;
+  const password = req.query.password;
+  return admin.database().ref('/participant').push({email: email, password: password}).then((snapshot) => {
+    return res.redirect(303, snapshot.ref);
+  });
+});
+
+
+/********************************************************************************************
+ * Function 1: Take the text parameter passed to this HTTP endpoint and insert it into the  *
+ *             Realtime Database under the path /messages/:pushId/original                  *
+ ********************************************************************************************/
+
